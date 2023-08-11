2a852ca0a954c4c00e6110374df6dfb9f8de993e
Jose A. Martinez <[200~jose.alfonso.martinez@gmail.com>
2023-08-11 00:58:28 -0500

    added the search for an existing escrow contract

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import deploy from './deploy';
 import Escrow from './Escrow';
 
+
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 export async function approve(escrowContract, signer) {
@@ -26,18 +27,21 @@ function App() {
     getAccounts();
   }, [account]);
 
+
   async function newContract() {
+    console.log("Entered newContract");
     const beneficiary = document.getElementById('beneficiary').value;
     const arbiter = document.getElementById('arbiter').value;
-    const value = ethers.BigNumber.from(document.getElementById('wei').value);
+//    const value = ethers.BigNumber.from(document.getElementById('wei').value);
+    const value = ethers.utils.parseEther(document.getElementById('wei').value);
     const escrowContract = await deploy(signer, arbiter, beneficiary, value);
-
 
     const escrow = {
       address: escrowContract.address,
       arbiter,
       beneficiary,
       value: value.toString(),
+      isApproved: false,
       handleApprove: async () => {
         escrowContract.on('Approved', () => {
           document.getElementById(escrowContract.address).className =
@@ -50,13 +54,64 @@ function App() {
       },
     };
 
+    console.log("newContract:setting escrows state variable", escrows, escrow)
     setEscrows([...escrows, escrow]);
+  }
+
+  async function loadExistingEscrowContract(){   
+    
+    const adr = document.getElementById('existingScrowContractAddress').value
+    const abi = [{"inputs":[{"internalType":"address","name":"_arbiter","type":"address"},{"internalType":"address","name":"_beneficiary","type":"address"}],"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"","type":"uint256"}],"name":"Approved","type":"event"},{"inputs":[],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"arbiter","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"beneficiary","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"depositor","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"isApproved","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]
+    let escrowContract = new ethers.Contract(adr, abi, provider);
+    /* 
+    //provider is a global variable defined at the beginning of this file
+
+    console.log('loadExistingEscrowContract address is', adr);
+    console.log('loadExistingEscrowContract abi is', abi);
+    console.log('provider=', provider);
+    console.log('ESCROW CONTRACT:');     
+    console.log('address=', adr);     
+    console.log('arbiter', await escrowContract.arbiter());
+    console.log('beneficiary', await escrowContract.beneficiary()); 
+    console.log('value', (await provider.getBalance(adr)).toString()); 
+    */
+    console.log('isApproved', await escrowContract.isApproved());
+
+    const escrow = {
+        address: adr,
+        arbiter: await escrowContract.arbiter(),
+        beneficiary: await escrowContract.beneficiary(),
+        value: (await provider.getBalance(adr)).toString(), 
+        isApproved: await escrowContract.isApproved(), 
+        handleApprove: async () => {
+          escrowContract.on('Approved', () => {
+            document.getElementById(escrowContract.address).className =
+              'complete';
+            document.getElementById(escrowContract.address).innerText =
+              "✓ It's been approved!";
+          });
+          if(!(await escrowContract.isApproved())) { 
+            await approve(escrowContract, signer);
+          }
+        },
+      };
+
+      setEscrows([...escrows, escrow]);
+  }
+
+  function validateEther() { 
+    try {
+      ethers.utils.parseEther(document.getElementById('wei').value);
+      return true; 
+    } catch (e) { 
+      return false; 
+    }
   }
 
   return (
     <>
-      <div className="contract">
-        <h1> New Contract </h1>
+      <div className="new-escrow-contract">
+        <h1> New Escrow Contract </h1>
         <label>
           Arbiter Address
           <input type="text" id="arbiter" />
@@ -68,7 +123,7 @@ function App() {
         </label>
 
         <label>
-          Deposit Amount (in Wei)
+          Deposit Amount (in Eth)
           <input type="text" id="wei" />
         </label>
 
@@ -77,20 +132,38 @@ function App() {
           id="deploy"
           onClick={(e) => {
             e.preventDefault();
-
-            newContract();
+            if(validateEther()) {
+              newContract();  
+            } else {
+              alert ('Amount missunderstanded');
+            }
           }}
         >
           Deploy
         </div>
-      </div>
+        <div className="secondOptionSeparator">or</div>
+         <label>
+          Existing Escrow Contract
+          <input type="text" id="existingScrowContractAddress" placeholder="Address of the deployed smart contract"/>
+        </label>
 
-      <div className="existing-contracts">
-        <h1> Existing Contracts </h1>
+        <div
+          className="button"
+          id="search"
+          onClick={(e) => {
+            e.preventDefault();
+            loadExistingEscrowContract();
+          }}
+        >
+          Search
+        </div>
+      </div>
+      <div className="contract-details">
+        <h1> Details </h1>
 
         <div id="container">
           {escrows.map((escrow) => {
-            return <Escrow key={escrow.address} {...escrow} />;
+            return <Escrow key={escrow.address} isApproved={escrow.isApproved} {...escrow} />;
           })}
         </div>
       </div>
--- app/src/Escrow.js
+++ app/src/Escrow.js
@@ -1,24 +1,87 @@
+import { ethers } from 'ethers';
+
 export default function Escrow({
   address,
   arbiter,
   beneficiary,
   value,
+  isApproved,
   handleApprove,
 }) {
-  return (
-    <div className="existing-contract">
-      <ul className="fields">
+  let approvedContainer = <div className="complete">✓ It has been approved!</div>;
+  if (isApproved) {  
+    return (
+      <div className="existing-contract">
+        <ul className="fields">
+          <li>
+            <div> Address </div>
+            <div> {address} </div>
+          </li>
+          <li>
+            <div> Arbiter </div>
+            <div> {arbiter} </div>
+          </li>
+          <li>
+            <div> Beneficiary </div>
+            <div> {beneficiary} </div>
+          </li>
+          <li>
+            <div> Value  </div>
+            <div> {ethers.utils.formatEther(value)}Eth</div> <div>({value}Wei)</div>
+          </li>
+          <li>
+            <div className="complete">✓ It has been approved!</div>
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="existing-contract">
+        <ul className="fields">
+          <li>
+            <div> Address </div>
+            <div> {address} </div>
+          </li>
+          <li>
+            <div> Arbiter </div>
+            <div> {arbiter} </div>
+          </li>
+          <li>
+            <div> Beneficiary </div>
+            <div> {beneficiary} </div>
+          </li>
+          <li>
+            <div> Value  </div>
+            <div> {ethers.utils.formatEther(value)}Eth</div> <div>({value}Wei)</div>
+          </li>
+          <li> <div className="button" 
+                                id={address}
+                                onClick={(e) => {
+                                e.preventDefault();
+                                handleApprove();
+                              }}
+                            >
+                            Approve
+                          </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+/* 
+//  '<div className="button" id={address}
+//                              onClick={(e) => {
+                                e.preventDefault();
+                                handleApprove();
+                              }}
+                            >
+                            Approve
+                          </div>}
         <li>
-          <div> Arbiter </div>
-          <div> {arbiter} </div>
-        </li>
-        <li>
-          <div> Beneficiary </div>
-          <div> {beneficiary} </div>
-        </li>
-        <li>
-          <div> Value </div>
-          <div> {value} </div>
+          <div> isApproved  </div>
+          <div>{isApproved ? 'Yes': 'No'}</div>
         </li>
         <div
           className="button"
@@ -31,7 +94,7 @@ export default function Escrow({
         >
           Approve
         </div>
-      </ul>
-    </div>
-  );
+
+*/
+
 }
